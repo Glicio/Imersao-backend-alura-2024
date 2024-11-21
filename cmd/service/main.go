97c975455fefc166211f4071e191aa2faff6cbec
Glicio Pereira Uchoa <glicioo@outlook.com>
2024-11-21 10:19:29 -0300

    removido types nao usado

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -8,19 +8,9 @@ import (
 	"github.com/Glicio/go-api-gemini/internal/database"
 )
 
-type Response struct {
-	Message string `json:"message"`
-	Status  string `json:"status"`
-}
-type Request struct {
-	Message string `json:"message"`
-  //optional key
-  Key string `json:"key,omitempty"`
-}
-
 func main() {
 	http.HandleFunc("/", api.Index)
-  http.HandleFunc("/api", api.Api)
+  http.HandleFunc("/posts", api.Posts)
   database.Init()
 	fmt.Println("Listening on port 3000")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
